a0ac74a5a7ce10262a7dc280f6a09f21da6aca21
Steven Tejeda <steventejeda@gmail.com>
2021-06-10 14:00:53 -0400

    Installing axios, adding a few components

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div className="coinSearch">
+            <h1 className="coin-text">Search a Currency</h1>
+                <form>
+                    <input type="text" placeholder="Search"
+                    className="coin-input"/>
+                </form>
+            </div>
+ 
+    )
+}
+
+export default Search
